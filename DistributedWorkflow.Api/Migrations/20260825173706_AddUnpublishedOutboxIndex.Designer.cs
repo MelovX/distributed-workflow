919d3dd667768fff770fec74e05c2bbfa25da916
Mikhail G. Mokin <74342632+MelovX@users.noreply.github.com>
2026-08-25 21:55:48 +0400

    perf: unpublished messages index added

--- DistributedWorkflow.Api/Migrations/20260825173706_AddUnpublishedOutboxIndex.Designer.cs
+++ DistributedWorkflow.Api/Migrations/20260825173706_AddUnpublishedOutboxIndex.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DistributedWorkflow.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DistributedWorkflow.Api.Migrations
 {
     [DbContext(typeof(RegistrationDbContext))]
-    partial class RegistrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260825173706_AddUnpublishedOutboxIndex")]
+    partial class AddUnpublishedOutboxIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DistributedWorkflow.Api/Migrations/20260825173706_AddUnpublishedOutboxIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DistributedWorkflow.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnpublishedOutboxIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessages_Unpublished_CreatedAt_Id",
+                table: "OutboxMessages",
+                columns: new[] { "CreatedAt", "Id" },
+                filter: "\"PublishedAt\" IS NULL")
+                .Annotation("Npgsql:CreatedConcurrently", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboxMessages_Unpublished_CreatedAt_Id",
+                table: "OutboxMessages");
+        }
+    }
+}
